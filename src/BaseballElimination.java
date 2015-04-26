f437cd21cadd6603fc8b478804682aebd4a10742
cuteme <pokalyaev.sv@mail.ru>
2015-04-27 02:14:35 +0300

    new SAP file

--- src/BaseballElimination.java
+++ src/BaseballElimination.java
@@ -1,6 +1,7 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.Arrays;
 
 /**
  * Created by Sergej.Pokalyaev on 24.04.2015.
@@ -11,36 +12,97 @@ public class BaseballElimination {
     int[] l;
     int[] r;
     int[][] g;
-    int teamNumbers;
+    int numberOfTeams;
     String[] teamNames;
 
-    public BaseballElimination(String filename) throws IOException {
+    private FlowNetwork flowNetwork;
 
-        BufferedReader reader = new BufferedReader(new FileReader(filename));
-        String line;
-        //List<String> lines = new ArrayList<String>();
-        line = reader.readLine();
-        teamNumbers = Integer.parseInt(line);
+    public BaseballElimination(String filename) {
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(filename));
+            String line;
+            line = reader.readLine();
+            numberOfTeams = Integer.parseInt(line);
 
-        w = new int[teamNumbers];
-        l = new int[teamNumbers];
-        r = new int[teamNumbers];
-        g = new int[teamNumbers][teamNumbers];
-        teamNames = new String[teamNumbers];
+            w = new int[numberOfTeams];
+            l = new int[numberOfTeams];
+            r = new int[numberOfTeams];
+            g = new int[numberOfTeams][numberOfTeams];
+            teamNames = new String[numberOfTeams];
 
-        while ((line = reader.readLine()) != null) {
+            for (int i = 0; i < numberOfTeams; i++) {
 
-            String[] elms = line.split(" ");
-            elms[]
+                line = reader.readLine();
 
+                String[] elms = line.split("\\s+");
+                teamNames[i] = elms[0];
+                w[i] = Integer.parseInt(elms[1]);
+                l[i] = Integer.parseInt(elms[2]);
+                r[i] = Integer.parseInt(elms[3]);
 
+                for (int j = 0; j < numberOfTeams; j++) {
+                    g[i][j] = Integer.parseInt(elms[j + 4]);
+                }
+            }
+        } catch (IOException ex) {
         }
+    }
 
+    public int numberOfTeams() {
+        return numberOfTeams;
+    }
 
+    public Iterable<String> teams() {
+        return Arrays.asList(teamNames);
+    }
+
+    public int wins(String team) {
+        return r[Arrays.asList(w).indexOf(team)];
+    }
+
+    public int losses(String team) {
+        return r[Arrays.asList(l).indexOf(team)];
+    }
+
+    public int remaing(String team) {
+        return r[Arrays.asList(teamNames).indexOf(team)];
+    }
+
+    public int against(String team1, String team2) {
+        return g[Arrays.asList(teamNames).indexOf(team1)][Arrays.asList(teamNames).indexOf(team2)];
+    }
+
+    public boolean isEliminated(String team) {
+        return false;
+    }
+
+    public Iterable<String> certificateOfElimination(String team) {
+        return null;
+
+    }
+
+    private void initFlowNet() {
+
+        int numberOfVertices = 1 + numberOfTeams + ((numberOfTeams - 1) / 2 ) * (numberOfTeams - 2) + (numberOfTeams - 1) % 2;
+        flowNetwork = new FlowNetwork(numberOfVertices);
 
     }
 
 
+    public static void main(String[] args) {
+
+        BaseballElimination baseballElimination = new BaseballElimination(args[0]);
+        //System.out.println(baseballElimination.remaing("Atlanta"));
+
+        baseballElimination.initFlowNet();
+
+
+
+        //flowNetwork = new FlowNetwork();
+
+
+
+    }
 
 
 
